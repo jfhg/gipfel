6c50749f40e50a4b6a260a262b40035bfe83fd76
Johannes Hofmann <Johannes.Hofmann@gmx.de>
2006-10-30 20:49:30 +0100

    add simplified copyright

--- src/ProjectionTangential.cxx
+++ src/ProjectionTangential.cxx
@@ -1,3 +1,8 @@
+//
+// Copyright 2006 Johannes Hofmann <Johannes.Hofmann@gmx.de>
+//
+// This software may be used and distributed according to the terms
+// of the GNU General Public License, incorporated herein by reference.
 
 #include <stdio.h>
 #include <stdlib.h>
