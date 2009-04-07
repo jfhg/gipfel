ceef96683481b96ab2dc418fee437a82251af88f
Johannes Hofmann <Johannes.Hofmann@gmx.de>
2009-04-07 18:53:59 +0200

    remove unnecessary includes

--- src/GipfelWidget.cxx
+++ src/GipfelWidget.cxx
@@ -9,10 +9,6 @@
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <errno.h>
 #include <math.h>
 #include <algorithm>
